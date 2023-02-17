38cc367d3111f5eb550fb268747bf47ab6647a4f
panagiotisfassaris <116089809+panagiotisfassaris@users.noreply.github.com>
2023-02-17 16:32:51 +0200

    Update form_setting.php

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,7 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from the site 'Panagiotis Fassaris'"; //letter subject
 	$to = 'panagiotisfassaris@gmail.com'; // Enter Your E-mail
-	$from = 'admin@panagiotisfassaris.com'; // Admin e-mail
+	$from = 'panagiotisfassaris@gmail.com'; // Admin e-mail
 	$fromName = 'Panagiotis Fassaris Site'; // Your company name
 	$charset = 'UTF-8';
 ?>
